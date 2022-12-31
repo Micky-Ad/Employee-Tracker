5ba2ce73f2dcb5ec0dada44c5e110592c0c716cf
Micky-Ad <110940136+Micky-Ad@users.noreply.github.com>
2022-12-31 11:01:12 -0500

    more of the functions to add and view database wokring for each

--- Main/server.js
+++ Main/server.js
@@ -1,4 +1,5 @@
 // Import and require inquirer
+import e from "express";
 import inquirer from "inquirer";
 // Import and require mysql2
 
@@ -11,7 +12,7 @@ const db = mysql.createConnection(
     // MySQL username,
     user: "root",
     // TODO: Add MySQL password here
-    password: "",
+    password: "2222",
     database: "employee_tracker_db",
   },
   console.log(`Connected to the employee_tracker database.`)
@@ -50,6 +51,10 @@ function menu() {
       veiwAllDepartments();
     } else if (choice == "View all roles") {
       veiwAllRoles();
+    } else if (choice == "Add employee") {
+      addEmployee();
+    } else if (choice == "Update Employee Role") {
+      updateEmployee();
     }
   });
 }
@@ -100,7 +105,7 @@ function addRole() {
     },
     {
       type: "input",
-      name: "departmant",
+      name: "department",
       message: "enter the role's Department Id",
     },
   ];
@@ -132,13 +137,115 @@ function veiwAllDepartments() {
 }
 
 function veiwAllRoles() {
-  var sql = "SELECT * FROM role ";
+  var sql =
+    "SELECT role.id,role.title, role.salary, department.name AS Department FROM role INNER JOIN department ON role.department_id = department.id ";
   db.query(sql, (err, res) => {
     if (err) {
       console.log(err);
     } else {
       console.table(res);
     }
+  });
+}
+
+function addEmployee() {
+  var question = [
+    {
+      type: "input",
+      name: "first_name",
+      message: "enter the first name of employee ",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "enter the last name of employee ",
+    },
+    {
+      type: "input",
+      name: "role_id",
+      message: "enter the role id of the employee",
+    },
+    {
+      type: "input",
+      name: "manager_id",
+      message: "enter the manager id for this employee",
+    },
+  ];
+  inquirer.prompt(question).then((answer) => {
+    console.log(answer);
+    var first_name = answer.first_name;
+    var last_name = answer.last_name;
+    var role_id = answer.role_id;
+    var manager_id = answer.manager_id;
+    if (manager_id == "null" || manager_id == "") {
+      manager_id = null;
+    }
+    var sql =
+      "INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)";
+    db.query(sql, [first_name, last_name, role_id, manager_id], (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Employee added to Database");
+      }
+    });
+  });
+}
+
+async function updateEmployee() {
+  var employeeList = await getEmployeeList();
+  var firstNames = [];
+  employeeList.forEach((employee) => {
+    firstNames.push(employee.first_name);
+  });
+
+  var question2 = [
+    {
+      type: "list",
+      name: "employee",
+      message: " Please select the employee that you want to update ",
+      choices: firstNames,
+    },
+  ];
+  inquirer.prompt(question2).then((answers2) => {
+    var firstName = answers2.employee;
+    var employeeId = null;
+    employeeList.forEach((employee) => {
+      if (employee.first_name == firstName) {
+        employeeId = employee.id;
+      }
+    });
+    var question = [
+      {
+        type: "input",
+        name: "role_id",
+        message: "enter the role id of the employee",
+      },
+    ];
+    inquirer.prompt(question).then((answer) => {
+      console.log(answer);
+
+      var role_id = answer.role_id;
+
+      var sql = "UPDATE employee SET role_id=? WHERE id=? ";
+      db.query(sql, [role_id, employeeId], (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Employee role updated on the Database");
+        }
+      });
+    });
+  });
+}
+
+//  Waiting for the query to excute and return the results
+async function getEmployeeList() {
+  return new Promise(function (resolve, reject) {
+    var sql = "SELECT * FROM employee ";
+    db.query(sql, (err, res) => {
+      resolve(res);
+    });
   });
 }
 runApp();
